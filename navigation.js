58c8fc6f09b2e41791f31cd3ae69b4e9b719c3d7
Usman Sahir <usman.sahir@carleton.ca>
2017-02-13 16:15:11 -0500

    Navbar/ BecomeACustomer page
    Navigation.js for have the navigationbar across the html pages. New
    structure for the becoming a customer form

---
+++ navigation.js
@@ -0,0 +1,9 @@
+document.getElementById("navMenu").innerHTML =
+
+'<ul>'+
+	'<li><a href="index.html">Home</a></li>'+
+	'<li><a href="javascript:void(0)">About</a></li>'+
+	'<li><a href="javascript:void(0)">Products</a></li>'+
+	'<li><a href="customer.html">Become A Customer</a></li>'+
+	'<li><a href="contactus.html">Contact Us</a><li>'+
+'</ul>';
