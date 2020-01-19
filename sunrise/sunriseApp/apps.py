35f2705fcec9d29de2dd0f1a4436eaf490fa588a
Youssef Halwani <yfhalwani@Youssefs-MacBook-Pro.local>
2020-01-19 02:16:13 -0500

    Updated views and templates

--- sunrise/sunriseApp/apps.py
+++ sunrise/sunriseApp/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class SunriseappConfig(AppConfig):
     name = 'sunriseApp'
--- sunrise/sunriseApp/urls.py
+++ sunrise/sunriseApp/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'sunriseApp'
 urlpatterns = [
+    # ex: /sunriseApp/
     path('', views.index, name='index'),
+    # ex: /sunriseApp/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /sunriseApp/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /sunriseApp/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
