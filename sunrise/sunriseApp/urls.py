225c3563646f9a07244a9d294049534d744f7434
Youssef Halwani <yfhalwani@Youssefs-MacBook-Pro.local>
2020-01-31 19:39:50 -0500

    Altered admin and views

--- sunrise/sunriseApp/urls.py
+++ sunrise/sunriseApp/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'sunriseApp'
 urlpatterns = [
     # ex: /sunriseApp/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /sunriseApp/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /sunriseApp/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /sunriseApp/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- sunrise/sunriseApp/views.py
+++ sunrise/sunriseApp/views.py
@@ -1,24 +1,53 @@
 # Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'sunriseApp/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'sunriseApp/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'sunriseApp/detail.html', {'question': question})
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'sunriseApp/results.html', {'question': question})
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'sunriseApp/index.html', context)
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'sunriseApp/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'sunriseApp/detail.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'sunriseApp/results.html'
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'sunriseApp/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
